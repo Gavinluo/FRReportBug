edbf8186052597af828511bf1816402f979ed763
gavin <gavinluo@qt-asia.com>
2022-03-04 20:33:44 +0800

    push demo

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM hub.360scm.cn/library/aspnet:3.1-libgdiplus-font AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:3.1 AS build
+WORKDIR /src
+COPY ["FR2022ReportBug.csproj", "."]
+RUN dotnet restore "./FR2022ReportBug.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "FR2022ReportBug.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "FR2022ReportBug.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "FR2022ReportBug.dll"]
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace FR2022ReportBug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fReport = new FastReport.Report();
+            fReport.Report.Load("NumberChineseDemo.frx");
+            DataSet ds = new DataSet("Table1");
+            DataTable dt = new DataTable("Table1");
+            dt.Columns.Add("ZWEIGHT", typeof(String));
+            dt.Columns.Add("ZVOLUME", typeof(String));
+            dt.Rows.Add("207.000000", "907.344000");
+            ds.Tables.Add(dt);
+            fReport.RegisterData(ds);
+            fReport.Prepare();
+            var exportPdf = new FastReport.Export.Pdf.PDFExport();
+            exportPdf.SetReport(fReport);
+            exportPdf.Compressed = true;
+            exportPdf.Background = false;
+            exportPdf.PrintOptimized = false;
+            exportPdf.OpenAfterExport = false;
+            string filename = DateTime.Now.ToString("MM-dd-HH-mm-ss") + ".pdf";
+            fReport.Export(exportPdf, filename);
+        }
+    }
+}

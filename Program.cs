31c88243002860d0ef1886041ac47a77718730f9
gavin <gavinluo@qt-asia.com>
2022-03-11 19:33:26 +0800

    date format bug report

--- Program.cs
+++ Program.cs
@@ -7,9 +7,34 @@ namespace FR2022ReportBug
     {
         static void Main(string[] args)
         {
-            TestNumberChinese();
+            TestDateFormat();
         }
-
+        /// <summary>
+        /// https://support.fast-report.com/tickets/680360
+        /// </summary>
+        public static void TestDateFormat()
+        {
+            var fReport = new FastReport.Report();
+            fReport.Report.Load("DatetimeFormatBug.frx");
+            DataSet ds = new DataSet("Table1");
+            DataTable dt = new DataTable("Table1");
+            dt.Columns.Add("CREATED_DATE", typeof(DateTime));
+            dt.Rows.Add(DateTime.Now);
+            ds.Tables.Add(dt);
+            fReport.RegisterData(ds);
+            fReport.Prepare();
+            var exportPdf = new FastReport.Export.Pdf.PDFExport();
+            exportPdf.SetReport(fReport);
+            exportPdf.Compressed = false;
+            exportPdf.Background = false;
+            exportPdf.PrintOptimized = false;
+            exportPdf.OpenAfterExport = false;
+            string filename = DateTime.Now.ToString("MM-dd-HH-mm-ss") + ".pdf";
+            fReport.Export(exportPdf, filename);
+        }
+        /// <summary>
+        /// https://support.fast-report.com/tickets/678371
+        /// </summary>
         public static void TestNumberChinese()
         {
             var fReport = new FastReport.Report();
